449d6e6294f8250190547598f84f7c6f128df65a
Akash Goswami <akash@fusioncharts.com>
2018-02-05 00:45:24 +0530

    Add comments in source

--- knn_algo.py
+++ knn_algo.py
@@ -2,22 +2,34 @@ import numpy as np
 
 class KNN:
     def __init__ (self, k):
-        """ Pass k and distanceFn as the hyperparameter"""
+        """ Pass k as the hyperparameter"""
         self.k = k
 
     def train(self, X, y):
-        """During training time stores the references of the input only"""
+        """
+            X is the example training matrix. Every row of X contains one training example. Each training example
+            may have `d` features. If there are `m` such examples then X is `m x d` matrix.
+            y is the label matrix corrosponding to each training example. Hence y is `m x 1` matrix.
+        """
+
+        # During training time stores the references of the input only
         self.tX = X
         self.ty = y
 
     def predict(self, X):
-        """predict a given X predit y"""
+        """
+            Predict y based on test data X.
+        """
+
         num_training = X.shape[0]
         YPred = np.zeros(num_training, dtype = self.ty.dtype)
 
         for i in range(num_training):
+            # Euclidean distance is used to find out the distance between two datapoint.
             distances = np.reshape(np.sqrt(np.sum(np.square(self.tX - X[i, :]), axis=1)), (-1, 1))
+            # Along with the distance stack the labels so that we can vote easily
             distance_label = np.hstack((distances, self.ty))
+            # Simple majority voting based on the minimum distance
             sorted_distance = distance_label[distance_label[:,0].argsort()]
             k_sorted_distance = sorted_distance[:self.k,:]
             (labels, occurence) = np.unique(k_sorted_distance[:, 1], return_counts=True)
